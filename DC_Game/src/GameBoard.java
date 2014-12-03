e1400dc9aab6fe49ffce74a2c14d64cfc2924d96
cahnda <cahnda@gmail.com>
2014-12-03 14:18:56 -0500

    Added Array Implementation of the Board

--- DC_Game/src/GameBoard.java
+++ DC_Game/src/GameBoard.java
@@ -3,33 +3,19 @@ import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("serial")
-public class GameBoard extends JPanel{	
+public class GameBoard extends JPanel{
 	public static final int BOARD_WIDTH = 300;
 	public static final int BOARD_HEIGHT = 300;
-	private Pawn wp1;
-	private Pawn wp2;
-	private Pawn wp3;
-	private Pawn wp4;
-	private Pawn wp5;
-	private Pawn wp6;
-	private Pawn wp7;
-	private Pawn wp8;
-	private King wk1;
-	private Queen wq1;
-	private Bishop wb1;
-	private Bishop wb2;
-	private Knight wn1;
-	private Knight wn2;
-	private Rook wr1;
-	private Rook wr2;
+	private GameObj[][] board_arrangement;
 	
 	
 	// Creates board every-time the code is run based on current status
 	public GameBoard() {
 		setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		/*
+		board_arrangement = new GameObj[8][8];
 		setLayout(new GridLayout(8,8));
 		setSize (BOARD_WIDTH,BOARD_HEIGHT);
+		/*
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				JPanel square = new JPanel();
@@ -48,25 +34,25 @@ public class GameBoard extends JPanel{
 	// Set the parameters when the board is created
 	public void reset() {
 		//Starting with zero for consistent with CS norms
-		wp1 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,0);
-		wp2 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,1);
-		wp3 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,2);
-		wp4 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,3);
-		wp5 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,4);
-		wp6 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,5);
-		wp7 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,6);
-		wp8 = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,1,7);
-		/*
-		wr1 = new Rook(BOARD_WIDTH,BOARD_HEIGHT,0,0);
-		wr2 = new Rook(BOARD_WIDTH,BOARD_HEIGHT,0,7);
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++){
+				if (i == 1) {
+					board_arrangement[i][j] = new Pawn(BOARD_WIDTH,BOARD_HEIGHT,i,j);
+				} else {
+					board_arrangement[i][j] = null;  
+					}
+				}
+			}
+		}
+	/*
+		wr1 = new Rook(BOARD_WIDTH,BOARD_HEIGHT,i,j);
 		wb1 = new Bishop(BOARD_WIDTH,BOARD_HEIGHT,0,2);
 		wb2 = new Bishop(BOARD_WIDTH,BOARD_HEIGHT,0,5);
 		wn1 = new Knight(BOARD_WIDTH,BOARD_HEIGHT,0,1);
 		wn2 = new Knight(BOARD_WIDTH,BOARD_HEIGHT,0,6);
 		wq1 = new Queen(BOARD_WIDTH,BOARD_HEIGHT,0,4);
 		wk1 = new King(BOARD_WIDTH,BOARD_HEIGHT,0,3);
-		
-		
+
 		Pawn bp1 = new Pawn();
 		Pawn bp2 = new Pawn();
 		Pawn bp3 = new Pawn();
@@ -84,32 +70,16 @@ public class GameBoard extends JPanel{
 		Queen bq1 = new Queen();
 		King bk1 = new King();
 		*/
-	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		wp1.draw(g);	wp2.draw(g);	wp3.draw(g);
-		wp4.draw(g);	wp5.draw(g);	wp6.draw(g);
-		wp7.draw(g);	wp8.draw(g);
-		
-		/*
-		wr1.draw(g); 	wr2.draw(g);
-		wb1.draw(g); 	wb2.draw(g);
-		wn1.draw(g); 	wn2.draw(g);
-		wq1.draw(g);
-		wk1.draw(g);
-		
-		
-		bp1.draw(g);	bp2.draw(g);	bp3.draw(g);
-		bp4.draw(g);	bp5.draw(g);	bp6.draw(g);
-		bp7.draw(g);	bp8.draw(g);
-		br1.draw(g); 	br2.draw(g);
-		bb1.draw(g); 	bb2.draw(g);
-		bn1.draw(g); 	bn2.draw(g);
-		bq1.draw(g);
-		bk1.draw(g);
-		*/
+		for (GameObj[] go_arr: board_arrangement) {
+			for (GameObj go: go_arr) {
+				if (!(go == null))
+					go.draw(	g);
+			}
+		}
 	}
 	
 	@Override
--- DC_Game/src/Rook.java
+++ DC_Game/src/Rook.java
@@ -1,7 +1,7 @@
 import java.awt.Graphics;
 
 
-public class Rook {
+public class Rook extends GameObj {
 
 	public Rook(int boardWidth, int boardHeight, int i, int j) {
 		// TODO Auto-generated constructor stub
